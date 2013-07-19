598443528bd1d23fa3c68543551ba117c0ae74e2
Ubuntu <ubuntu@domU-12-31-39-10-0A-16.compute-1.internal>
2013-07-19 09:22:11 +0000

    Static files

--- web.js
+++ web.js
@@ -3,7 +3,6 @@ var bufer = fs.readFileSync("index.html",'utf8');
 
 var express = require('express');
 
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
@@ -12,6 +11,7 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.PORT || 8080;
+app.use(express.static(__dirname +''));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
